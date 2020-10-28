50a89be80d77719e3cacd9df36bcfb50d26607d9
4current <r.morrisey@computer.org>
2020-10-27 20:49:17 -0400

    examining settingd

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/4current/radmail
 
 go 1.15
+
+require (
+	github.com/fsnotify/fsnotify v1.4.9
+	github.com/spf13/viper v1.7.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"reflect"
+	"sync"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+func waitForCtrlC() {
+	var endWaiter sync.WaitGroup
+	endWaiter.Add(1)
+	var signalChannel chan os.Signal
+	signalChannel = make(chan os.Signal, 1)
+	signal.Notify(signalChannel, os.Interrupt)
+	go func() {
+		<-signalChannel
+		endWaiter.Done()
+	}()
+	endWaiter.Wait()
+}
 
 func main() {
-	fmt.Println("Hello")
+
+	v := viper.New()
+	cfgDir := "$HOME/Dropbox/ham/Winlink/pat_mail/.wl2k"
+	v.SetConfigName("config")
+	v.SetConfigType("json")
+	v.AddConfigPath(cfgDir)
+	err := v.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	v.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("A File Event happened: ", e.Name, e.Op)
+	})
+	v.WatchConfig()
+
+	var settings map[string]interface{}
+	settings = v.AllSettings()
+	viper.Unmarshal(&settings)
+
+	fmt.Println(reflect.TypeOf(settings))
+
+	for k, v := range settings {
+		fmt.Println("key: ", k, "    value: ", reflect.TypeOf(v))
+	}
+
+	fmt.Printf("Press Ctrl+C to end\n")
+	waitForCtrlC()
+	fmt.Printf("\n")
+
 }
